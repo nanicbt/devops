68e5d0abcdeae35d695439f2c7e6524ba6f3e976
nanicbt <11mjnkr@gmail.com>
2024-11-24 12:48:28 +0530

    Loggers Added

--- src/main/java/com/devops/DevopsApplication.java
+++ src/main/java/com/devops/DevopsApplication.java
@@ -1,13 +1,25 @@
 package com.devops;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class DevopsApplication {
+	
+	private static final Logger loggerr = LoggerFactory.getLogger(DevopsApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(DevopsApplication.class, args);
+	
+	@PostConstruct
+	public void init() {
+		loggerr.info("Before Application Context Initializing");
 	}
 
+	public static void main(String[] args) {
+		loggerr.info("Application Context Loading...");
+		SpringApplication.run(DevopsApplication.class, args);
+	}
 }
--- src/test/java/com/devops/DevopsApplicationTests.java
+++ src/test/java/com/devops/DevopsApplicationTests.java
@@ -1,13 +1,21 @@
 package com.devops;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DevopsApplicationTests {
 
+	private static final Logger loggerr = LoggerFactory.getLogger(DevopsApplicationTests.class);
+	
 	@Test
 	void contextLoads() {
+		loggerr.info("Testing Cases Running..");
+		assertEquals(true, true);
 	}
 
 }
